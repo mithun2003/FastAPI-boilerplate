22576512d41190b01c0dda92d17bd68e241c833b
Mithun Thomas <63800900+mithun2003@users.noreply.github.com>
2024-04-30 10:00:53 +0530

    check the path of env

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -7,6 +7,7 @@ from starlette.config import Config
 current_file_dir = os.path.dirname(os.path.realpath(__file__))
 env_path = os.path.join(current_file_dir, "..", "..", ".env")
 config = Config(env_path)
+print(env_path, config)
 
 
 class AppSettings(BaseSettings):
